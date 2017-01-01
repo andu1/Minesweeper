4807f4504e78788adfc422f68096fc5c8d528b3a
andu1 <aaaxux@gmail.com>
2017-01-01 15:15:43 -0600

    Minesweeper

---
+++ src/minesweeper/Game.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minesweeper;
+
+/**
+ *
+ * @author wsxbi
+ */
+public class Game {
+    private Board board;
+    boolean finish = false;
+    boolean win = false;
+    int turn=0;
+    
+    public Game(){
+        board = new Board();
+        Play(board);
+    }
+    
+    public void Play(Board board){
+        do{
+            turn++;
+            System.out.println("Turn "+turn);
+            board.show();
+            finish = board.setPosition();
+            
+            if(!finish){
+                board.openNeighbors();
+                finish = board.win();
+            }
+            
+        }while(!finish);
+        
+        if(board.win()){
+            System.out.println("Congratulations, you let the 10 fields with the mines in "+turn+" turns");
+            board.showMines();
+        } else {
+            System.out.println("Mine! You lost!");
+            board.showMines();
+        }
+    }
+}
---
+++ src/minesweeper/Minesweeper.java
@@ -0,0 +1,9 @@
+package minesweeper; 
+
+public class Minesweeper {
+    public static void main(String[] args) {
+        Game game = new Game();
+
+    }
+
+}
